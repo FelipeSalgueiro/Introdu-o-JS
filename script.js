72f212173da1185d122a33b9bb30344d6813067c
FelipeSalgueiro <felipemaiasalgueiro@gmail.com>
2024-10-23 19:39:30 -0300

    :tada: Primeiro Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let mesAtual = "Outubro"
+const mesesDoAno = 12
+
+let primeiroNome = ""
+let sobrenome = ""
+
+let inverno = false
+
+let botaoAtivo = true
+
+if (botaoAtivo == true){
+    console.log("Botão está ativo!")
+}
+
+let maiorDeIdade = false
+if (maiorDeIdade == true){
+    console.log("Bem Vindo!")
+}
+else {
+    console.log("Volte em 4 anos!")
+}
